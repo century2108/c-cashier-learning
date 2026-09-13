f4a26cdcb59a34c6bf52411c8ba148efd3f99c12
century2108 <centuryyg@outlook.com>
2026-09-13 23:09:50 +0800

    1.1.5(结束)修改了输入prices没有结束而继续走流程到报错的问题，把表格行距修改并且增加了分割线

--- level1/level1.1.c
+++ level1/level1.1.c
@@ -8,13 +8,12 @@ char code1[3][100]={"1","2","3"};//code1是code的字符串形式,后面用户�
 float price[3] ={3.50,0.50,6.00};
 
 char MC[20];//MC表输入的名称
-//int SZ=0;//SZ代表输入的数字
-//scanf("%d",&SZ)
+
 
 int i;
 while(1)
 {
-printf("Enter product name or code:\n (tips1:you can enter 'exit' or 'quit' to quit）\n（tips2:you can enter 'prices' to view all products）\n(tips3:product names are Cola, Lollipop, Noodles）\n");//提示用户输入名称或名称和如何关闭程序
+printf("Enter product name or code:\n (tips1:you can enter 'exit' or 'quit' to quit）\n（tips2:you can enter 'prices' to view all products）\n(tips3:product names are Cola, Lollipop, Noodles）\n");//提示用户输入名称或代码和如何关闭程序
 scanf("%s",MC);
 
 if(strcmp(MC,"exit")==0 || strcmp(MC,"quit")==0)
@@ -22,16 +21,21 @@ if(strcmp(MC,"exit")==0 || strcmp(MC,"quit")==0)
 break;
 }
 if(strcmp(MC,"prices")==0)
-{
+{printf("%-15s  %-15s  %-15s\n","name","code","price");
+    printf("--------------------------------------------------\n");//不知道除了printf----还有没有更好的方法来打印表格，太慢了
 for(i=0;i<3;i++)
 {
-printf("Product: %s, Code: %d, Price: %.2f\n", name[i], code[i], price[i]);
+    
+printf("%-15s  %-15d  %-15.2f\n", name[i], code[i], price[i]);
+  
 }
+break;
 }
 
 for(i=0;i<3;i++)                                                       
 {
 //if(code[i]==SZ)
+
 if(strcmp(name[i],MC)==0 || strcmp(code1[i],MC)==0)
 {printf("code:%d\nprice:%.2f\n",code[i],price[i]);
 break;

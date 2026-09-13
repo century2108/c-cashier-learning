3872363783b7a06491855e2280b230d1686a024d
century2108 <centuryyg@outlook.com>
2026-09-13 21:19:07 +0800

    1.1.1输入数组，定义商品名称编号和价格，并先尝试for循环，也完成报错板块的编写

---
+++ level1/level1.1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+char name[3][100]={"Cola","Lollipop","Noodles"};
+int code[3]={1,2,3};
+float price[3] ={3.50,0.50,6.00};
+
+
+int SZ=0;//SZ代表输入的数字
+scanf("%d",&SZ);
+int i;
+
+for(i=0;i<3;i++)                                                       
+{
+if(code[i]==SZ)
+{printf("ok");
+break;
+}
+};
+if(i == 3)
+{printf("ERROR :code not found\n");
+
+
+}
+
+    return 0;
+}

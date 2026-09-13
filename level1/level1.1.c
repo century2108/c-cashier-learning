ff97d3f28ba170cc153867a3c2d6b6953dc7a550
century2108 <centuryyg@outlook.com>
2026-09-13 21:40:35 +0800

    1.1.2把对代码的检索更改对名称的，为让输入字符串与已有数据库匹配，引入strcmp

--- level1/level1.1.c
+++ level1/level1.1.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 int main()
 {
 char name[3][100]={"Cola","Lollipop","Noodles"};
 int code[3]={1,2,3};
 float price[3] ={3.50,0.50,6.00};
 
-
-int SZ=0;//SZ代表输入的数字
-scanf("%d",&SZ);
+char MC[20];//MC表输入的名称
+//int SZ=0;//SZ代表输入的数字
+//scanf("%d",&SZ)
+scanf("%s",MC);
 int i;
 
 for(i=0;i<3;i++)                                                       
 {
-if(code[i]==SZ)
+//if(code[i]==SZ)
+if(strcmp(name[i],MC)==0)
 {printf("ok");
 break;
 }

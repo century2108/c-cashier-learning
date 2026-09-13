90a5ba14cd1fbc234f4d9b49815819f938517362
century2108 <centuryyg@outlook.com>
2026-09-13 22:38:13 +0800

    让用户可以输入商品名称或编号来查询相关信息，贴合秋招题

--- level1/level1.1.c
+++ level1/level1.1.c
@@ -4,6 +4,7 @@ int main()
 {
 char name[3][100]={"Cola","Lollipop","Noodles"};
 int code[3]={1,2,3};
+char code1[3][100]={"1","2","3"};//code1是code的字符串形式,后面用户既可输入名称也可输入编号查找相关信息
 float price[3] ={3.50,0.50,6.00};
 
 char MC[20];//MC表输入的名称
@@ -13,7 +14,7 @@ char MC[20];//MC表输入的名称
 int i;
 while(1)
 {
-printf("Enter product name:\n (tips1:you can enter 'exit' or 'quit' to quit）\n（tips2:you can enter 'prices' to view all products）\n(tips3:product names are Cola, Lollipop, Noodles\n)");//提示用户输入名称和如何关闭程序
+printf("Enter product name or code:\n (tips1:you can enter 'exit' or 'quit' to quit）\n（tips2:you can enter 'prices' to view all products）\n(tips3:product names are Cola, Lollipop, Noodles）\n");//提示用户输入名称或名称和如何关闭程序
 scanf("%s",MC);
 
 if(strcmp(MC,"exit")==0 || strcmp(MC,"quit")==0)
@@ -31,7 +32,7 @@ printf("Product: %s, Code: %d, Price: %.2f\n", name[i], code[i], price[i]);
 for(i=0;i<3;i++)                                                       
 {
 //if(code[i]==SZ)
-if(strcmp(name[i],MC)==0)
+if(strcmp(name[i],MC)==0 || strcmp(code1[i],MC)==0)
 {printf("code:%d\nprice:%.2f\n",code[i],price[i]);
 break;
 }

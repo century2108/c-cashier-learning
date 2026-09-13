9a3922583f5719f78df78cd7867aa4e43cb2210e
century2108 <centuryyg@outlook.com>
2026-09-13 22:23:10 +0800

    1.1.4加入输入prices查询所有内容的功能，并且完善tips

--- level1/level1.1.c
+++ level1/level1.1.c
@@ -13,12 +13,19 @@ char MC[20];//MC表输入的名称
 int i;
 while(1)
 {
-printf("Enter product name:\n tips:you can enter 'exit' or 'quit' to quit\n");//提示用户输入名称和如何关闭程序
+printf("Enter product name:\n (tips1:you can enter 'exit' or 'quit' to quit）\n（tips2:you can enter 'prices' to view all products）\n(tips3:product names are Cola, Lollipop, Noodles\n)");//提示用户输入名称和如何关闭程序
 scanf("%s",MC);
 
 if(strcmp(MC,"exit")==0 || strcmp(MC,"quit")==0)
 {
 break;
+}
+if(strcmp(MC,"prices")==0)
+{
+for(i=0;i<3;i++)
+{
+printf("Product: %s, Code: %d, Price: %.2f\n", name[i], code[i], price[i]);
+}
 }
 
 for(i=0;i<3;i++)                                                       

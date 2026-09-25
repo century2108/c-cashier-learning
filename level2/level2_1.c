63870529b683801ec695dfd5334306042180ef2a
century2108 <centuryyg@outlook.com>
2026-09-26 00:27:13 +0800

    2_1.1加入管理员模式，管理员模式初步加入可修改密码和返回用户模式功能

--- level2/level2_1.c
+++ level2/level2_1.c
@@ -3,7 +3,8 @@
 #include <time.h>
 int today_date = 1;
 int serial_no = 0;
-
+int admin_mode = 0;
+char admin_password[100] = "admin123";
 int main()
 {
     char name[3][100] = {"Cola", "Lollipop", "Noodles"};
@@ -17,8 +18,68 @@ int main()
     int i;
     while (1)
     {
-        printf("Enter product name or code:\n (tips1:you can enter 'exit' or 'quit' to quit)\n(tips2:you can enter 'prices' to view all products)\n(tips3:product names are Cola, Lollipop, Noodles)\n"); // 提示用户输入名称或代码和如何关闭程序
+        if (admin_mode == 1)
+        {
+            printf("(tips1:可输入'setpassword+_+新密码'来修改密码)\n");
+            printf("(tips2:可输入'back'返回用户模式)\n");
+            printf("admin>");
+        }
+        else
+        {
+            printf(">");
+            printf("Enter product name or code:\n (tips1:you can enter 'exit' or 'quit' to quit)\n(tips2:you can enter 'prices' to view all products)\n(tips3:product names are Cola, Lollipop, Noodles)\n"); // 提示用户输入名称或代码和如何关闭程序
+        }
+
         scanf("%s", MC);
+        if (strcmp(MC, "admin") == 0)
+        {
+            char input_password[100];
+            printf("Password:\n");
+            scanf("%s", input_password);
+            if (strcmp(input_password, admin_password) == 0)
+
+            {
+                admin_mode = 1;
+                printf("Admin mode.\n");
+                continue;
+            }
+
+            else
+            {
+                printf("Error:wrong password.\n");
+                continue;
+            } // 判断是否进入管理员模式，是则继续，不是则提示用户输入名称或代码
+        }
+
+        if (MC[0] == 's' && MC[1] == 'e' && MC[2] == 't' && MC[3] == 'p' && MC[4] == 'a' && MC[5] == 's' && MC[6] == 's' && MC[7] == 'w' && MC[8] == 'o' && MC[9] == 'r' && MC[10] == 'd' && MC[11] == '_')
+        {
+            if (admin_mode == 1)
+            {
+                // MC+12：从下标12位置开始，就是新密码字符串
+                strcpy(admin_password, MC + 12);
+                printf("Password changed.\n");
+                continue;
+            }
+            else
+            {
+                printf("ERROR: 你没有管理员权限\n");
+            }
+            continue;
+        }
+
+        if (strcmp(MC, "back") == 0)
+        {
+            if (admin_mode == 1)
+            {
+                admin_mode = 0;
+                printf("Bye.\n");
+            }
+            else
+            {
+                printf("ERROR: 你没有管理员权限\n");
+            }
+            continue;
+        }
 
         if (strcmp(MC, "exit") == 0 || strcmp(MC, "quit") == 0)
         {
